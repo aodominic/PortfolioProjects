4bf317ba384e8a3a510c52357b364985ba3677ab
aodominic <71844359+aodominic@users.noreply.github.com>
2023-06-22 14:01:47 -0700

    CovidData.sql

--- CovidData.sql
+++ CovidData.sql
@@ -15,22 +15,11 @@ where continent is not null
 --Group By date
 order by 1,2
 
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
-
-
---Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
---From PortfolioProject..CovidDeaths
-----Where location like '%states%'
---where location = 'World'
-----Group By date
---order by 1,2
-
 
 -- 2. 
 
 -- We take these out as they are not inluded in the above queries and want to stay consistent
--- European Union is part of Europe
+-- EU is part of Europe
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
